2e79e9224d9c4202ee2762fbe62a6d3a3a9311f8
AnastasyMeleshko <7634907@mail.ru>
2022-05-21 19:46:43 +0300

    feat: add solution for task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const { readdir, stat } = require('fs').promises;
+const path = require('path');
+const { stdout } = process;
+
+(async() =>  {
+  const folderPath = path.join(__dirname, 'secret-folder');
+
+  try {
+    const files = await readdir(folderPath, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        const stats = await stat(path.join(folderPath, file.name));
+        stdout.write(`${path.parse(file.name).name} - ${path.extname(file.name).slice(1)} - ${stats.size}b\n`);
+      }
+    }
+  } catch (error) {
+    stdout.write(error.message);
+  }
+})();
